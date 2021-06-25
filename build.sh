d9862b1d40eb9dc20d83946e265563c49d1089ce
Ethan Weed <weed.ethan@gmail.com>
2021-06-25 09:17:42 +0200

    moved slides for testing

--- build.sh
+++ build.sh
@@ -14,4 +14,6 @@ pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/_build/html/Sli
 pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/_build/html/Syllabus.html
 
 # Push to github pages
-ghp-import -n -p -f _build/html
+#ghp-import -n -p -f _build/html
+
+ghp-import -p -f _build/html
