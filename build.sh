e48cd6ea9c18c86d8a87482f50c63a06457cced7
Ethan Weed <weed.ethan@gmail.com>
2021-06-25 10:37:09 +0200

    moved gh-pages to master

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@
 
 
 # Slides
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/_build/html/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 
 
 
 
 # Other
-pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/_build/html/Syllabus.html
+pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/Syllabus.html
 
 # Push to github pages
 #ghp-import -n -p -f _build/html
 
 
-ghp-import -p -f _build/html
+#ghp-import -p -f _build/html
