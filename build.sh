38c48a392d27b6826c23e168071a4cfee3eff6c8
Ethan Weed <weed.ethan@gmail.com>
2021-08-20 09:54:28 +0200

    auto updated with build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/S
 #Lab slides
 #pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
 
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V theme=simple -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V theme=sky -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 	
 
 
