21c0cd7eb2d2074926999b71e0e0a1d5b47c3d66
Ethan Weed <weed.ethan@gmail.com>
2021-08-20 11:24:13 +0200

    auto updated with build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 
 
 # Lecture slides
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lecture_02.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
 
 
 #Lab slides
 #pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
 
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab_02_Why.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 	
 
 
