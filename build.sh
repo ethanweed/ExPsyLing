f745e20239cc8a1546ab59d4a0759ef6a8ae5975
Ethan Weed <weed.ethan@gmail.com>
2021-08-20 09:41:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,18 @@
 # build html documents
 
 
+# Lecture slides
+pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
 
-# Slides
+
+#Lab slides
 pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Lab1_Why.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
 	
 
 
 #pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/#Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/#Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/
 
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Slides/Slides_week_02.md -V revealjs-url=https://unpkg.com/reveal.js@^4/
+
 
 # Other
 pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
@@ -20,6 +23,6 @@ pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedu
 
 # Push to github
 
-#git add -A
-#git commit -m "auto updated with build.sh"
-#git push origin master
+git add -A
+git commit -m "auto updated with build.sh"
+git push origin master
