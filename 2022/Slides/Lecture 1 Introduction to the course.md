5cd8356b6c75546643f4eda9f24d3728d62a83fa
ethanweed <weed.ethan@gmail.com>
2022-08-11 15:15:36 +0200

    auto-updated with build.sh

---
+++ 2022/Slides/Lecture 1 Introduction to the course.md
@@ -0,0 +1,34 @@
+# Title
+
+---
+
+# Review
+
+---
+
+## Quiz
+
+---
+
+# Topic 1
+
+---
+
+# Topic 2
+
+---
+
+
+# References
+
+---
+
+## References
+
+
+<div id = "refs">
+
+
+
+
+</div>
---
+++ 2022/buildslides.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+## Lecture Slides
+
+cd /Users/ethan/Documents/GitHub/ExPsyLing/2022/Slides/
+
+for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=styles.css $i -o html/"${i%.*}".html ; done
+
+for i in *.html; do mv $i /html; done;
+
+# Push to github
+
+git add -A
+git commit -m "auto-updated with build.sh"
+git push origin master
