8a7cf5da5a709511641e3192d556b04a5abf865a
ethanweed <weed.ethan@gmail.com>
2022-08-16 12:33:20 +0200

    auto-updated with build.sh

--- build.sh
+++ build.sh
@@ -1,95 +1,19 @@
 #!/bin/bash
 
-# build html documents
 
-# in case you need it:
-#-V revealjs-url=https://unpkg.com/reveal.js@^4/
+## Course Planning
+pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2022/Teaching Flow.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2022/Teaching Flow.html
 
-# Notebooks
 
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_02.ipynb
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_02_solutions.ipynb
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_04.ipynb
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_04_assignment.ipynb
+## Lecture Slides
 
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/datasets/Sternberg/scripts/SternbergPlotGallery.ipynb
+cd /Users/ethan/Documents/GitHub/ExPsyLing/2022/Slides/
 
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_08_t-tests.ipynb
-
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_Report_02_starter.ipynb
-
-jupyter nbconvert /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_08_t-tests-solutions.ipynb --to html 
-
-jupyter nbconvert /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/ANOVA_notebook1.ipynb --to html 
-
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_Report_02_starter2.ipynb
-
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_Report_02_Solutions.ipynb
-
-jupyter nbconvert --to html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Notebooks/Lab_Report_03_Solutions.ipynb
+for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/ --include-in-header=styles.css $i -o html/"${i%.*}".html ; done
 
 
 
 
-####### Lecture slides
-
-# Lecture 2
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_02_Intro.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 3
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_03_Psycholinguistics.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_03_Psycholinguistics.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 4
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_04_WorkingMemory.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_04_WorkingMemory.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 5
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_05_ResearchDesign.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_05_ResearchDesign.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 6
-pandoc -t revealjs  --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_06_DescriptiveStatistics.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_06_DescriptiveStatistics.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 7
-pandoc -t revealjs  --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_07_Hypothesis-Testing.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_07_Hypothesis-Testing.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 8
-pandoc -t revealjs  --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_08_Lexical-Access.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_08_Lexical-Access.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 9
-pandoc -t revealjs  --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_09_Sternberg_revisited.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_09_Sternberg_revisited.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-# Lecture 10
-pandoc -t revealjs  --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_10_SemanticOrganization.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lecture_10_SemanticOrganization.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-
-
-#Lab slides
-
-
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_02_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_02_Why.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
-
-pandoc -t revealjs -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_02_Why.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_02_Why.md -V theme=solarized -V revealjs-url=https://unpkg.com/reveal.js/
-
-pandoc -t revealjs --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_06_VisualizingData.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_06_VisualizingData.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-pandoc -t revealjs --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_07_Sternberg-Design.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_07_Sternberg-Design.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-pandoc -t revealjs --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_08_t-tests.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_08_t-tests.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-pandoc -t revealjs --mathjax -s -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_09_Lexical_decision_tasks.html /Users/ethan/Documents/GitHub/ExPsyLing/2021/Slides/Lab_09_Lexical_decision_tasks.md -V theme=solarized -V transition=none -V revealjs-url=https://unpkg.com/reveal.js/
-
-
-
-
-#Assignments
-
-
-
-
-
-# Other
-pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Syllabus.html
-
-pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.md -o /Users/ethan/Documents/GitHub/ExPsyLing/2021/Planning/Schedule.html
 
 # Push to github
 
