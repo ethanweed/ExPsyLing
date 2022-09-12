4d71ce56e2570f9e5c9b20ac5433c4033e4c8d91
ethanweed <weed.ethan@gmail.com>
2022-09-12 21:58:09 +0200

    auto-updated with build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pandoc --standalone /Users/ethan/Documents/GitHub/ExPsyLing/2022/Syllabus-2022.m
 
 cd /Users/ethan/Documents/GitHub/ExPsyLing/2022/Slides/
 
-for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/ --filter pandoc-mermaid --include-in-header=styles.css $i -o html/"${i%.*}".html ; done
+for i in *.md ; do echo "$i" && pandoc metadata.yaml -s -t revealjs -s -V revealjs-url=https://unpkg.com/reveal.js/  --include-in-header=styles.css $i -o html/"${i%.*}".html --filter pandoc-mermaid; done
 
 
 
